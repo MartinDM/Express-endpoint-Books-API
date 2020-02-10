bda1b90056d083ca67b28a6a6815d90ff20f3f94
martinDM <hello@martindm.uk>
2020-02-10 17:23:51 +0000

    Endpoint for Stripe POST

--- app.js
+++ app.js
@@ -6,24 +6,11 @@ const stripe = require('stripe')(process.env.SK);
 // Secret Key from Stripe dashboard
 
 const app = express();
-if ( process.env.ENV == 'Test') {
-  console.log('Dev');
-  const db = mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds137008.mlab.com:37008/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
-} else {
-  console.log('Prod');
-  const db = mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds137008.mlab.com:37008/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-
 const port = process.env.PORT || 3000;
-
-// Books API
-const Book = require('./models/bookModel');
-const bookRouter = require('./routes/bookRouter')(Book); 
 
 // Body-parser adds request to req.body
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-app.use('/api', bookRouter);
 
 app.server = app.listen(port, () => {
   // Returns the server which is listening
