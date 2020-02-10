fe6948e17278e82dee97fa9f71ea32260f103cfd
martinDM <hello@martindm.uk>
2020-02-10 17:07:27 +0000

    Stripe endpoint: POST to /stripe

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const stripe = require('stripe')(process.env.SK);
+
 require('dotenv').config();
 const app = express();
-
 if ( process.env.ENV == 'Test') {
   console.log('Dev');
   const db = mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds137008.mlab.com:37008/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,17 +14,27 @@ if ( process.env.ENV == 'Test') {
 }
 
 const port = process.env.PORT || 3000;
+
+app.post('/intents', async (req, res) => {
+  console.log(req.body)
+  const { amount, currency } = req.body;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency,
+    payment_method_types: ['card']
+  });
+  res.send(paymentIntent);
+});
+
+// Books API
 const Book = require('./models/bookModel');
-const bookRouter = require('./routes/bookRouter')(Book);
+const bookRouter = require('./routes/bookRouter')(Book); 
 
 // Body-parser adds request to req.body
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use('/api', bookRouter);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the books API!');
-});
 
 app.server = app.listen(port, () => {
   // Returns the server which is listening
